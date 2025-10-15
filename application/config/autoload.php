44cd66986dea055323e2ccc8435a198745635672
ziapark <ziapark320@gmail.com>
2025-10-15 17:24:52 +0900

    fix helper url 추가

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('template_', 'session', 'optimizer', 'database');
+$autoload['libraries'] = array('template_', 'session', 'optimizer', 'database', 'pagination');
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
