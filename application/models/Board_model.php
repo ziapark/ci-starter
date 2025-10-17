2be054ed7fb7e4d6e7927c68e4391f44be0b02f1
ziapark <ziapark320@gmail.com>
2025-10-17 16:25:53 +0900

    fix 카테고리

--- application/models/Board_model.php
+++ application/models/Board_model.php
@@ -15,6 +15,22 @@
             return $result;
         }
 
+        //카테고리별 게시글 목록
+        public function get_board_list_category($limit_per_page, $offset, $current_category){
+            $query = $this->db->query('
+                select board.*, user.u_id, category.category_name
+                from board
+                join user on board.u_num = user.u_num
+                join category on board.category_idx = category.category_idx
+                where board.category_idx = ?
+                order by board.b_date desc
+                limit ? offset ?;
+            ', [$current_category, $limit_per_page, $offset]);
+
+            $result = $query->result();
+            return $result;
+        }
+
         //전체 게시글 수
         public function count_all_boards(){
             return $this->db->count_all('board');
@@ -33,12 +49,12 @@
         }
 
         //게시글 등록
-        public function insert_board($u_num, $b_title, $b_content){  
+        public function insert_board($u_num, $b_title, $b_content, $category_idx){  
             $result = $this->db->query('
                 insert into board
-                (u_num, b_title, b_content)
-                values (?, ?, ?) 
-            ', [$u_num, $b_title, $b_content]);
+                (u_num, b_title, b_content, category_idx)
+                values (?, ?, ?, ?) 
+            ', [$u_num, $b_title, $b_content, $category_idx]);
 
             return $result;
         }
@@ -46,14 +62,14 @@
         //게시글 정보
         public function get_board_detail($b_num){
             $query = $this->db->query('
-                select board.*, user.u_id
+                select board.*, user.u_id, category.category_name
                 from board
                 join user on board.u_num = user.u_num
+                join category on board.category_idx = category.category_idx
                 where b_num=?', [$b_num]);
             $row = $query->row();
 
             return $row;
-    
         }
 
         //게시글 삭제
@@ -66,25 +82,36 @@
         }
 
         //게시글 수정
-        public function update($b_num, $b_title, $b_content){
+        public function update($b_num, $b_title, $b_content, $category_idx){
             $query = $this->db->query('
                 update board
-                set b_title=?, b_content=?
-                where b_num=?', [$b_title, $b_content, $b_num]);
+                set b_title=?, b_content=?, category_idx=?
+                where b_num=?', [$b_title, $b_content, $category_idx, $b_num]);
             return ;
         }
 
         //키워드 검색
-        public function search($keyword, $limit_per_page, $offset){
+        public function search($keyword, $limit_per_page, $offset, $current_category){
             $like = "%{$keyword}%";
 
-            $query = $this->db->query("
-                select board.*, user.u_id
-                from board
-                join user on board.u_num = user.u_num
-                where board.b_title like ?
-                order by board.b_date desc limit ? offset ?", [$like, $limit_per_page, $offset]);
-            
+            if($current_category != 'all'){
+                $query = $this->db->query("
+                    select board.*, user.u_id
+                    from board
+                    join user on board.u_num = user.u_num
+                    where board.b_title like ? and board.category_idx = ?
+                    order by board.b_date desc limit ? offset ?
+                ", [$like, $current_category, $limit_per_page, $offset]);
+            }else{
+                $query = $this->db->query("
+                    select board.*, user.u_id
+                    from board
+                    join user on board.u_num = user.u_num
+                    where board.b_title like ?
+                    order by board.b_date desc limit ? offset ?
+                ", [$like, $limit_per_page, $offset]);
+            }   
+
             $result = $query->result();
             return $result;
         }
